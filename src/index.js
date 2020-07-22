26facdfff29990353682b86854e42f025296a83f
Nikita Shabulin <nwpress@yandex.ru>
2020-07-23 00:29:14 +0700

    Добавлено открытие меню + адаптив для него

--- src/index.js
+++ src/index.js
@@ -1,1 +1,2 @@
 import './modules/togglePhone';
+import './modules/toggleMenu';
---
+++ src/modules/toggleMenu.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const toggleMenu = () => {
+  const menuIcon = document.querySelector('.menu__icon'),
+    menuPopup = document.querySelector('.popup.popup-menu'),
+    menuDialog = menuPopup.querySelector('.popup-dialog-menu');
+
+  let resolution = window.innerWidth;
+
+  const showMenu = () => {
+    menuPopup.style.visibility = 'visible';
+    
+    if (resolution > 576) {
+      menuDialog.style.transform = 'translate3d(0, 0, 0)';
+    } else {
+      menuDialog.style.transform = 'translate3d(0, 0, 0)';
+    }
+  };
+
+  const closeMenu = () => {
+    menuPopup.style.visibility = 'hidden';
+    
+    if (resolution > 576) {
+      menuDialog.style.transform = 'translate3d(100%, 0, 0)';
+    } else {
+      menuDialog.style.transform = 'translate3d(0, -100%, 0)';
+    }
+  };
+
+  window.addEventListener('resize', () => resolution = window.innerWidth);
+
+  menuIcon.addEventListener('click', showMenu);
+
+  menuPopup.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target.className === 'close close-menu') {
+      closeMenu();
+    }
+  });
+};
+
+toggleMenu();
